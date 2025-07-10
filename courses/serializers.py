b30883a6c3b710c62994ec7f87ec1ec2f1095aad
Samuel M. Debela <113627346+skdebela@users.noreply.github.com>
2025-07-10 07:08:07 +0300

    add instructor and is_enrolled to course

--- courses/serializers.py
+++ courses/serializers.py
@@ -1,12 +1,27 @@
 from rest_framework import serializers
 
 from courses.models import Content, Course, Enrollment, Progress
+from users.serializers import UserSerializer
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    instructor = UserSerializer(read_only=True)
+    is_enrolled = serializers.SerializerMethodField()
+
     class Meta:
         model = Course
         fields = "__all__"
+
+    def get_is_enrolled(self, obj) -> bool:
+        request = self.context.get("request")
+        if not request.user or not request.user.is_authenticated:
+            print("no user")
+            return False
+        if not hasattr(obj, "enrollments"):
+            print("no users")
+            return False
+        print("no user")
+        return obj.enrollments.filter(user__id=request.user.id).exists()
 
 
 class ContentSerializer(serializers.ModelSerializer):
